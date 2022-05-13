27eddad50815f46e84c99c32a610cf7b83cd0dcc
szilG <gombiszilvi@gmail.com>
2022-05-13 16:54:49 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          bye there
-        </p>
+        <h1>
+          Hello There Sleeepyhead!
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
